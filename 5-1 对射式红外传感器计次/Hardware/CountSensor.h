0799164af451bff9be5d937bb57fe8e6ce6dbb65
liangjiehub <2876146167@qq.com>
2026-04-09 14:02:25 +0800

    3-1 LED闪烁

---
+++ 5-1 对射式红外传感器计次/Hardware/CountSensor.h
@@ -0,0 +1,7 @@
+#ifndef _COUNT_SENSOR_H_
+#define _COUNT_SENSOR_H_
+
+void CountSensor_Init(void);
+uint16_t CountSensor_Get(void);
+
+#endif 
---
+++ 6-3 PWM驱动LED呼吸灯/Hardware/PWM.h
@@ -0,0 +1,8 @@
+#ifndef _PWM_H_
+#define _PWM_H_
+
+#include "stdint.h"
+void PWM_Init(void);
+void PWM_SetCompare1(uint16_t Compare);
+
+#endif
